e6eb627b7735ef5ce4c4924549234256347c88f2
Ziang LIU <50853393+zi-ang-liu@users.noreply.github.com>
2024-09-25 14:19:33 +0900

    a

--- test.c
+++ test.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-    int dt_1, dt_2, sum;
-    printf("Input number 1:");
-    scanf("%d", &dt_1);
-    printf("Input number 2:");
-    scanf("%d", &dt_2);
-    sum = dt_1 + dt_2;
-    printf("SUM = %d\n", sum);
+    int dt;
+    scanf("%d", &dt);
+    dt = dt * 2;
+    printf("%d\n", dt);
     return 0;
 }
